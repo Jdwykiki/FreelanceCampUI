8bd51fdc2783bbe178e5f900db8fa785c7138b11
Justin Wykstra <justinwykstra@gmail.com>
2016-11-15 16:10:33 -0700

    Added new files

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomepageComponent} from './homepage.commponent';
+
+@NgModule({
+	imports: [
+	Browsermodule
+	]
+})
+
+export class AppModule {}
---
+++ app/homepage.component.ts
@@ -0,0 +1,7 @@
+import { component } from '@angular/core';
+
+@component({
+	selector: 'main-app', 
+	template: '<h1>Freelance Bootcamp Homepage</h1>'
+})
+export class HomepageComponent {}
